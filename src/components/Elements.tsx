feb6ef50670b2dcfde4dccfcf72fd5ce8c6564c1
ashakirzianov <ashakirzianov@icloud.com>
2018-11-24 17:45:32 +0400

    Set default font family to 'Georgia'

--- src/components/Elements.tsx
+++ src/components/Elements.tsx
@@ -3,10 +3,12 @@ import { Text, View, Link as AtomLink } from './Atoms';
 import { Comp, size } from './comp-utils';
 import { FlexStyle } from 'react-native';
 
+const defaultFontFamily = 'Georgia';
 export const TextBlock: Comp<{ text: string }> = props =>
     <Text style={{
         fontSize: 20,
         textAlign: 'justify',
+        fontFamily: defaultFontFamily,
     }}>{'\t' /*
             React Native is missing text-indent styling
         */}{props.text}</Text>;
@@ -32,22 +34,22 @@ export const Row: Comp = props =>
 
 export const ChapterTitle: Comp<{ text?: string }> = props =>
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 20 }}>{props.text}</Text>
+        <Text style={{ fontSize: 20, fontFamily: defaultFontFamily }}>{props.text}</Text>
     </View>;
 
 export const PartTitle: Comp<{ text?: string }> = props =>
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <Text style={{ fontWeight: 'bold', fontSize: 24 }}>{props.text}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 24, fontFamily: defaultFontFamily }}>{props.text}</Text>
     </View>;
 
 export const SubpartTitle: Comp<{ text?: string }> = props =>
     <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
-        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{props.text}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 20, fontFamily: defaultFontFamily }}>{props.text}</Text>
     </View>;
 
 export const BookTitle: Comp<{ text?: string }> = props =>
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <Text style={{ fontWeight: 'bold', fontSize: 28 }}>{props.text}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 28, fontFamily: defaultFontFamily }}>{props.text}</Text>
     </View>;
 
 export {
