d038dda91b2200cb0fea46fa301e478c9d06159d
ashakirzianov <ashakirzianov@icloud.com>
2018-11-24 17:10:54 +0400

    Define debug backend base url

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { Book, noBook, errorBook, BookLocator, Library } from "../model";
 
-const backendBase = 'https://reader-back.herokuapp.com/';
+export const backendBaseProd = 'https://reader-back.herokuapp.com/';
+export const backendBaseDebug = 'http://localhost:3042/';
+const backendBase = backendBaseProd; // TODO: set depending on env var
 const jsonPath = 'json/';
 const libraryApi = 'library';
 
