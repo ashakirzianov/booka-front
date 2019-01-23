44e70eb721fe6240b68db116e7273ae0683dbdb2
Anton Shakirzianov <ashakirzianov@Antons-MacBook-Pro.local>
2019-01-23 16:55:02 -0300

    Fixed 'schedule/tracking' issue

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+
+import { AppComp } from './src/components';
+import { store } from "./src/redux";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello mobile</Text>
-      </View>
+      <Provider store={store}><AppComp /></Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
