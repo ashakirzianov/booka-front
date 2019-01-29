baaa1e67c66e3416f350002a7d8cfb498221f79d
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-01-29 15:51:45 -0300

    Put book locator into book

--- src/model/book.ts
+++ src/model/book.ts
@@ -1,3 +1,5 @@
+import { BookLocator, noBookLocator, errorBookLocator } from './bookLocator';
+
 export type Paragraph = string;
 export type Chapter = {
     book: "chapter",
@@ -15,16 +17,19 @@ export type BookMeta = {
 
 export type ActualBook = {
     book: "book",
+    locator: BookLocator,
     meta: BookMeta,
     content: BookNode[],
 };
 
 export type NoBook = {
     book: 'no-book',
+    locator: BookLocator,
 };
 
 export type ErrorBook = {
     book: 'error',
+    locator: BookLocator,
     error: string,
 };
 
@@ -33,15 +38,18 @@ export type Book = ActualBook | NoBook | ErrorBook;
 export function noBook(): NoBook {
     return {
         book: 'no-book',
+        locator: noBookLocator(),
     };
 }
 
 export function errorBook(error: string): ErrorBook {
     return {
         book: 'error',
+        locator: errorBookLocator(),
         error: error,
     };
 }
+
 // Type guards:
 
 export function isParagraph(bn: BookNode): bn is Paragraph {
--- src/model/bookLocator.ts
+++ src/model/bookLocator.ts
@@ -1,14 +1,25 @@
 export type NoBookLocator = { bl: 'no-book' };
+export type ErrorBookLocator = { bl: 'error-book' } // TODO: rethink and, perhaps, remove?
 export type StaticBookLocator = {
     bl: 'static-book',
     name: string,
 };
 
-export type BookLocator = NoBookLocator | StaticBookLocator;
+export type BookLocator = 
+    | NoBookLocator
+    | StaticBookLocator
+    | ErrorBookLocator
+    ;
 
 export function noBookLocator(): NoBookLocator {
     return {
         bl: 'no-book',
+    };
+}
+
+export function errorBookLocator(): ErrorBookLocator {
+    return {
+        bl: 'error-book',
     };
 }
 
