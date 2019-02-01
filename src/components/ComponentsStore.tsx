8c56495fe99cd427b0c346d714eb60b711b2d647
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-02-01 19:22:49 -0300

    Implement 'ElementsStore'

---
+++ src/components/ComponentsStore.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {
+    Label, ParagraphText, Row, Column, Screen,
+} from './Elements';
+
+export const ElementsStore: React.SFC = props => (
+    <Screen color='green'>
+        <ParagraphText text='Paragraph' />
+        <Row>
+            <Column>
+                <Label text='R 1 C 1' />
+                <Label text='R 2 C 1' />
+                <Label text='R 3 C 1' />
+            </Column>
+            <Column>
+                <Label text='R 1 C 2' />
+                <Label text='R 2 C 2' />
+                <Label text='R 3 C 2' />
+            </Column>
+        </Row>
+        <Column align='center'>
+            <Row>
+                <Label text='R 1 C 1' />
+                <Label text='R 1 C 2' />
+                <Label text='R 1 C 3' />
+            </Row>
+            <Row>
+                <Label text='R 2 C 1' />
+                <Label text='R 2 C 2' />
+                <Label text='R 2 C 3' />
+            </Row>
+        </Column>
+    </Screen>
+);
