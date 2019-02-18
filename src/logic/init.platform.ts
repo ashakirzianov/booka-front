625f4e12615f89f40806382e08b87efa4a3f4b56
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-02-18 16:57:56 -0300

    Do not 'push' browsing history on scroll

--- src/logic/init.platform.ts
+++ src/logic/init.platform.ts
@@ -1,27 +1,44 @@
 import { createBrowserHistory } from 'history';
 import { Middleware } from 'redux';
 import { dispatchNavigationEvent } from './urlNavigation';
-import { App, topScreen, blToString } from '../model';
+import { App, topScreen, blToString, pointToSameBook } from '../model';
 import { assertNever } from '../utils';
 
 const history = createBrowserHistory();
 export function onInit() {
     dispatchNavigationEvent(history.location.pathname);
     history.listen((location, action) => {
-        if (action === 'REPLACE') {
+        if (action !== 'REPLACE') {
             dispatchNavigationEvent(location.pathname);
         }
     });
 }
 
 export const updateHistoryMiddleware: Middleware<{}, App> = store => next => action => {
+    const prevState = store.getState();
     const result = next(action);
-    const url = stateToUrl(store.getState());
+    const currState = store.getState();
+    const url = stateToUrl(currState);
     if (history.location.pathname !== url) {
-        history.push(url);
+        if (needToReplace(prevState, currState)) {
+            history.replace(url);
+        } else {
+            history.push(url);
+        }
     }
     return result;
 };
+
+function needToReplace(prevState: App, currState: App): boolean {
+    const prevTop = topScreen(prevState.screenStack);
+    const currTop = topScreen(currState.screenStack);
+
+    if (prevTop && currTop && prevTop.screen === 'book' && currTop.screen === 'book') {
+        return pointToSameBook(prevTop.bl, currTop.bl);
+    } else {
+        return false;
+    }
+}
 
 function stateToUrl(state: App) {
     const top = topScreen(state.screenStack);
