becc705c2deb08f0c3ead129d06cc49e6d82ff2a
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-02-21 16:02:09 -0300

    TocScreenComp

--- src/components/ScreenComp.tsx
+++ src/components/ScreenComp.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 
-import { Screen, BookScreen, LibraryScreen } from '../model';
+import { Screen, BookScreen, LibraryScreen, TocScreen } from '../model';
 import { BookComp } from './BookComp';
 import { LibraryComp } from './LibraryComp';
-import { assertNever, throwExp } from '../utils';
+import { assertNever } from '../utils';
 import { Comp, comp, connected } from './comp-utils';
 import { navigateToBl } from '../logic';
-import { BookScreenLayout, LibraryScreenLayout } from './ScreenComp.Layout';
+import { BookScreenLayout, LibraryScreenLayout, TocScreenLayout } from './ScreenComp.Layout';
+import { TableOfContentsComp } from './TableOfContentsComp';
 
 export const ScreenComp: Comp<Screen> = (props =>
     props.screen === 'book' ? <BookScreenComp {...props} />
         : props.screen === 'library' ? <LibraryScreenComp {...props} />
-            : props.screen === 'toc' ? throwExp('Not implemented')
+            : props.screen === 'toc' ? <TocScreenComp {...props} />
                 : assertNever(props)
 );
 
@@ -25,4 +26,10 @@ const LibraryScreenComp = comp<LibraryScreen>(props =>
     <LibraryScreenLayout>
         <LibraryComp {...props.library} openBook={bl => navigateToBl(bl)} />
     </LibraryScreenLayout>
+);
+
+const TocScreenComp = comp<TocScreen>(props =>
+    <TocScreenLayout>
+        <TableOfContentsComp {...props.toc} />
+    </TocScreenLayout>
 );
