b5527db2e2f63314467f8530718063afbc4ff5da
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-03-25 19:30:27 -0300

    Introduce 'Theme' model element

---
+++ src/model/theme.ts
@@ -0,0 +1,10 @@
+export type FontFamily = 'Georgia';
+export type FontSize = number;
+export type Color = string;
+
+export type Theme = {
+    fontFamily: FontFamily,
+    foregroundColor: Color,
+    backgroundColor: Color,
+    baseFontSize: FontSize,
+};
