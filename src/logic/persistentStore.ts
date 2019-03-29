c39aba384de5e439de6f69c974228e6372982a8c
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-03-29 14:36:55 -0300

    Move default theme definition to model

--- src/logic/persistentStore.ts
+++ src/logic/persistentStore.ts
@@ -2,7 +2,7 @@ import {
     readValue,
     // storeValue,
 } from './persistentStore.platform';
-import { App, libraryScreen, library, Theme } from '../model';
+import { App, libraryScreen, library, defaultTheme } from '../model';
 
 const storeKey = 'state';
 export function storeState(state: App) {
@@ -20,44 +20,6 @@ function restoreState(): object | undefined {
 function validateState(restored: object | undefined): App | undefined {
     return undefined; // TODO: implement
 }
-
-export const lightTheme: Theme = {
-    fontFamily: 'Georgia',
-    palette: {
-        foreground: '#5f3e24',
-        background: '#f9f3e9',
-        secondBack: '#e6e0d6',
-        accent: '#987',
-        highlight: '#321',
-        shadow: '#000',
-    },
-    fontSize: {
-        normal: 26,
-        large: 30,
-        largest: 36,
-    },
-    fontScale: 1,
-    radius: 9,
-};
-export const darkTheme: Theme = {
-    fontFamily: 'Georgia',
-    palette: {
-        foreground: '#999',
-        background: '#000',
-        secondBack: '#222',
-        accent: '#ddd',
-        highlight: '#fff',
-        shadow: '#222',
-    },
-    fontSize: {
-        normal: 26,
-        large: 30,
-        largest: 36,
-    },
-    fontScale: 1,
-    radius: 9,
-};
-const defaultTheme: Theme = lightTheme;
 
 function createNewState(): App {
     return {
--- src/model/theme.ts
+++ src/model/theme.ts
@@ -22,3 +22,44 @@ export type Theme = {
     fontSize: FontSizes,
     radius: number,
 };
+
+export const whitePalette: Palette = {
+    foreground: '#000',
+    background: '#fff',
+    secondBack: '#eee',
+    accent: '#777',
+    highlight: '#aaf',
+    shadow: '#000',
+};
+
+export const sepiaPalette: Palette = {
+    foreground: '#5f3e24',
+    background: '#f9f3e9',
+    secondBack: '#e6e0d6',
+    accent: '#987',
+    highlight: '#321',
+    shadow: '#000',
+};
+
+export const darkPalette: Palette = {
+    foreground: '#999',
+    background: '#000',
+    secondBack: '#222',
+    accent: '#ddd',
+    highlight: '#fff',
+    shadow: '#222',
+};
+
+const defaultPalette = whitePalette;
+
+export const defaultTheme: Theme = {
+    fontFamily: 'Georgia',
+    palette: defaultPalette,
+    fontSize: {
+        normal: 26,
+        large: 30,
+        largest: 36,
+    },
+    fontScale: 1,
+    radius: 9,
+};
