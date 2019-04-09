cf1eba06da761bf7adb5aa86e45756297cce5df9
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-04-09 18:34:44 -0300

    ActivityIndicator component

--- src/blocks/Complex.tsx
+++ src/blocks/Complex.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { themed, palette } from './comp-utils';
-import { View } from 'react-native';
+import { View, ActivityIndicator as NativeActivityIndicator } from 'react-native';
 import { platformValue } from '../utils';
 
 export * from './Complex.platform';
@@ -16,4 +16,15 @@ export const Layer = themed(props =>
     }}>
         {props.children}
     </View>,
+);
+
+export const ActivityIndicator = themed(props =>
+    <NativeActivityIndicator size='large' style={{
+        position: 'absolute',
+        minHeight: '100%',
+        minWidth: '100%',
+        width: platformValue({ mobile: '100%' }),
+        height: platformValue({ mobile: '100%' }),
+        backgroundColor: palette(props).primary,
+    }} />,
 );
