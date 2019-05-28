a36c984d4d1477bcba98ab87e4470f867d8f1c6e
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-05-28 15:59:52 +0300

    Remove unnecessary connect

--- src/components/BookScreenComp.tsx
+++ src/components/BookScreenComp.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 import {
-    connect, connectActions, Row, relative, Clickable, Modal, PanelLink,
-    Comp, WithPopover, Line, Column, Link, PlainText, hoverable, View, Separator,
+    connectActions, Row, relative, Clickable, Modal, PanelLink,
+    Comp, WithPopover, Line, Column, Link, PlainText, hoverable, View, Separator, connectState,
 } from '../blocks';
 import { BookScreen, Book, BookId, TableOfContents, PaletteName, BookRange, FootnoteSpan, footnoteForId } from '../model';
 import { BookNodesComp } from './Reader';
@@ -157,7 +157,7 @@ const PalettePicker: Comp = (() =>
 );
 
 const HoverableView = hoverable(View);
-const PaletteButton = connect(['theme'], ['setPalette'])<{
+const PaletteButton = connectState('theme')<{
     name: PaletteName,
     text: string,
 }>(props => {
