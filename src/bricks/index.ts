a9a7e903ef5e77de3d03eddfc113a5dccbcbe526
ahshakirzianov <ashakirzianov@icloud.com>
2019-07-16 17:17:59 -0700

    Introducing 'bricks' and 'blocks'

---
+++ src/bricks/index.ts
@@ -0,0 +1,20 @@
+import * as Scroll from './Scroll';
+
+export type RefType = Scroll.RefType;
+
+export * from './utils';
+export { isOpenNewTabEvent, hoverable } from './utils';
+export {
+    isPartiallyVisible, scrollToRef, refable,
+} from './Scroll';
+export * from './Elements';
+export * from './Complex.common';
+export * from './Icons';
+export * from './common';
+export { SafeAreaView, View } from 'react-native';
+export { Row, Column, Scroll } from './Atoms';
+export {
+    TopBar, BottomBar, Clickable, DottedLine, LinkButton,
+    Modal, Separator, Tab, WithPopover, headerHeight,
+} from './Complex';
+export * from './RichText';
--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -14,8 +14,7 @@ import {
 import { BookSelection } from './Reader.common';
 import { buildNodes, buildBook, Params } from './bookRender';
 import { pathToString, parsePath } from './common';
-import { Clickable } from '../blocks';
-import { headerHeight } from '../blocks/Complex';
+import { Clickable, headerHeight } from '../blocks';
 
 type RefMap = { [k in string]?: RefType };
 export type ReaderProps = {
