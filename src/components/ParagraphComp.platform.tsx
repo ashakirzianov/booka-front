1d54bcb408b609d5e253a6af25ed08c9c5f28886
ashakirzianov <ashakirzianov@icloud.com>
2019-07-18 14:10:50 -0700

    Justify text

--- src/components/ParagraphComp.platform.tsx
+++ src/components/ParagraphComp.platform.tsx
@@ -3,7 +3,10 @@ import { Size, Props } from '../blocks';
 
 // TODO: do we need 'textIndent' ?
 export function ParagraphContainer(props: Props<{ textIndent: Size }>) {
-    return <div style={{ display: 'flex' }}>
+    return <div style={{
+        display: 'flex',
+        textAlign: 'justify',
+    }}>
         <span style={{
             float: 'left',
             textIndent: props.textIndent,
