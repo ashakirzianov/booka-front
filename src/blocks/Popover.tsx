5cd1f1cf868cb13f77e342f0928509fe1e63264f
ashakirzianov <ashakirzianov@icloud.com>
2019-07-25 15:03:25 -0700

    Reimplement web popover using hooks

--- src/blocks/Popover.tsx
+++ src/blocks/Popover.tsx
@@ -14,63 +14,50 @@ export type WithPopoverProps = {
     children: (onClick: Callback<void>) => React.ReactNode,
 };
 
-type WithPopoverState = {
-    open: boolean,
-};
-export class WithPopover extends React.Component<WithPopoverProps, WithPopoverState> {
-    public state = { open: false };
+export function WithPopover({ body, popoverPlacement, theme, children }: WithPopoverProps) {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const hide = () => setIsOpen(false);
 
-    public toggleVisibility() {
-        this.setState({ open: !this.state.open });
-    }
-
-    public hide() {
-        this.setState({ open: false });
-    }
-
-    public render() {
-        const { body, popoverPlacement, children } = this.props;
-        const { open } = this.state;
-        return <Manager>
-            <Reference>
-                {({ ref }) =>
-                    <>
-                        {!open ? null :
-                            <div style={{
-                                position: 'fixed',
-                                top: 0, bottom: 0, left: 0, right: 0,
-                                zIndex: -10,
-                            }}
-                                onClick={this.hide.bind(this)}
-                            />
-                        }
-                        <div ref={ref} style={{ display: 'flex' }}>
-                            {children(this.toggleVisibility.bind(this))}
-                        </div>
-                    </>
-                }
-            </Reference>
-            <FadeIn visible={open}>
-                <Popper
-                    placement={popoverPlacement}
-                    positionFixed={platformValue({
-                        firefox: true,
-                        default: false,
-                    })}
-                >
-                    {
-                        ({ ref, style, placement }) =>
-                            <div ref={ref} style={{
-                                ...style,
-                            }} data-placement={placement}>
-                                {/* TODO: add arrows */}
-                                <OverlayBox theme={this.props.theme}>
-                                    {body}
-                                </OverlayBox>
-                            </div>
+    return <Manager>
+        <Reference>
+            {({ ref }) =>
+                <>
+                    {!isOpen ? null :
+                        <div style={{
+                            position: 'fixed',
+                            top: 0, bottom: 0, left: 0, right: 0,
+                            zIndex: -10,
+                        }}
+                            onClick={hide}
+                        />
                     }
-                </Popper>
-            </FadeIn>
-        </Manager >;
-    }
+                    <div ref={ref} style={{ display: 'flex' }}>
+                        {children(toggle)}
+                    </div>
+                </>
+            }
+        </Reference>
+        <FadeIn visible={isOpen}>
+            <Popper
+                placement={popoverPlacement}
+                positionFixed={platformValue({
+                    firefox: true,
+                    default: false,
+                })}
+            >
+                {
+                    ({ ref, style, placement }) =>
+                        <div ref={ref} style={{
+                            ...style,
+                        }} data-placement={placement}>
+                            {/* TODO: add arrows */}
+                            <OverlayBox theme={theme}>
+                                {body}
+                            </OverlayBox>
+                        </div>
+                }
+            </Popper>
+        </FadeIn>
+    </Manager >;
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 
 import { ConnectedProvider, TestProvider } from '../core';
 import { TopComp } from './TopComp';
