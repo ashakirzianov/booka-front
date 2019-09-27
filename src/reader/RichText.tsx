d2b51113501a4a67fdf00df037bb367543ed393f
ashakirzianov <ashakirzianov@icloud.com>
2019-09-27 19:14:09 +0700

    Register refs in 'RichText'

--- src/reader/RichText.tsx
+++ src/reader/RichText.tsx
@@ -29,6 +29,7 @@ export type RichTextProps = {
     fontFamily: string,
 };
 export function RichText({ blocks, color, fontSize, fontFamily }: RichTextProps) {
+    const refMap = React.useRef<PathMap<RefType>>(makePathMap());
     return <span style={{
         color: color,
         fontSize: fontSize,
@@ -39,15 +40,33 @@ export function RichText({ blocks, color, fontSize, fontFamily }: RichTextProps)
                 <RichTextBlock
                     key={idx}
                     block={block}
+                    refCallback={(ref, offset) => {
+                        refMap.current.set([idx, offset], ref);
+                    }}
                 />
         )}
     </span>;
 }
 
+type RefType = HTMLSpanElement | null;
 type RichTextBlockProps = {
     block: RichTextBlock,
+    refCallback: (ref: RefType, offset: number) => void,
 };
-function RichTextBlock({ block }: RichTextBlockProps) {
+function RichTextBlock({ block, refCallback }: RichTextBlockProps) {
+    const children: JSX.Element[] = [];
+    let currentOffset = 0;
+    for (let idx = 0; idx < block.fragments.length; idx++) {
+        const frag = block.fragments[idx];
+        const offset = currentOffset;
+        children.push(<RichTextFragment
+            key={idx}
+            fragment={frag}
+            refCallback={ref => refCallback(ref, offset)}
+        />);
+        currentOffset += frag.text.length;
+    }
+
     return <div style={{
         display: 'flex',
         textAlign: 'justify',
@@ -55,21 +74,18 @@ function RichTextBlock({ block }: RichTextBlockProps) {
         textIndent: '4em',
     }}>
         <span>
-            {block.fragments.map(
-                (frag, idx) =>
-                    <RichTextFragment
-                        fragment={frag}
-                        key={idx}
-                    />)}
+            {children}
         </span>
     </div>;
 }
 
 type RichTextFragmentProps = {
     fragment: RichTextFragment,
+    refCallback: (ref: RefType) => void,
 };
-function RichTextFragment({ fragment: { text, attrs } }: RichTextFragmentProps) {
+function RichTextFragment({ fragment: { text, attrs }, refCallback }: RichTextFragmentProps) {
     return <span
+        ref={refCallback}
         style={{
             wordBreak: 'break-word',
             color: attrs.color,
@@ -93,4 +109,63 @@ function RichTextFragment({ fragment: { text, attrs } }: RichTextFragmentProps) 
     >
         {text}
     </span>;
+}
+
+type PathMap<T> = {
+    get(path: number[]): T | undefined,
+    set(path: number[], value: T): void,
+    iterator(): IterableIterator<[number[], T]>,
+};
+
+function makePathMap<T>(): PathMap<T> {
+    type MapNode = {
+        [idx: number]: {
+            value?: T,
+            children: PathMap<T>,
+        } | undefined,
+    };
+    const node: MapNode = {};
+    return {
+        get(path) {
+            if (path.length === 0) {
+                return undefined;
+            }
+            const head = node[path[0]];
+            if (path.length === 1) {
+                return head && head.value;
+            } else {
+                return head && head.children.get(path.slice(1));
+            }
+        },
+        set(path, value) {
+            if (path.length === 0) {
+                return;
+            }
+            let head = node[path[0]];
+            if (head === undefined) {
+                head = {
+                    children: makePathMap(),
+                };
+                node[path[0]] = head;
+            }
+            if (path.length === 1) {
+                head.value = value;
+            } else {
+                head.children.set(path.slice(1), value);
+            }
+        },
+        iterator: function* () {
+            for (const [key, value] of Object.entries(node)) {
+                const idx = parseInt(key, 10);
+                if (value) {
+                    if (value.value) {
+                        yield [[idx], value.value];
+                    }
+                    for (const [chPath, chValue] of value.children.iterator()) {
+                        yield [[idx, ...chPath], chValue];
+                    }
+                }
+            }
+        },
+    };
 }
